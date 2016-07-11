4bdfd0b9e9fb3fc51469704b24f57e6a96f9324b
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2016-07-11 16:55:44 +0100

    Allow changing the DPI profile with the replayer.
    This provides a flag to the replayer to set the DPI and builds the
    structure so that that it can be changed by the user. Use the default that
    the original application uses as well.

--- anker-mouse-replayer/replayer.go
+++ anker-mouse-replayer/replayer.go
@@ -35,6 +35,9 @@ import (
 var (
 	profile1Light = flag.String("profile1_light", "#0000ff:2:0", "String as color:brightness:breath for the light for profile #1.")
 	profile2Light = flag.String("profile2_light", "#00ff00:2:0", "String as color:brightness:breath for the light for profile #2.")
+
+	profile1DPI = flag.String("profile1_dpi", "1000,2000,4000,8200", "Comma-separated list of DPI values. Separate X:Y values with a colon for split-DPI; give an empty value to disable that DPI level (e.g. 1000:800,2000:1600,,).")
+	profile2DPI = flag.String("profile2_dpi", "1000,2000,4000,8200", "Comma-separated list of DPI values. Separate X:Y values with a colon for split-DPI; give an empty value to disable that DPI level (e.g. 1000:800,2000:1600,,).")
 )
 
 const (
@@ -66,6 +69,45 @@ func parseLightFlag(v string) (*colorful.Color, byte, byte, error) {
 	return &c, byte(bright), byte(breath), nil
 }
 
+func parseDPIFlag(v string) (*[4][2]int, error) {
+	p := strings.Split(v, ",")
+	if len(p) != 4 {
+		return nil, fmt.Errorf("Not enough DPI setting: %v", v)
+	}
+
+	var dpi = [4][2]int{}
+	for i := range p {
+		if p[i] == "" {
+			dpi[i][0] = 0
+		} else {
+			xy := strings.Split(p[i], ":")
+			switch len(xy) {
+			case 1:
+				xydpi, err := strconv.Atoi(xy[0])
+				if err != nil {
+					return nil, err
+				}
+				dpi[i] = [2]int{xydpi, xydpi}
+			case 2:
+				xdpi, err := strconv.Atoi(xy[0])
+				if err != nil {
+					return nil, err
+				}
+				ydpi, err := strconv.Atoi(xy[1])
+				if err != nil {
+					return nil, err
+				}
+
+				dpi[i] = [2]int{xdpi, ydpi}
+			default:
+				return nil, fmt.Errorf("Invalid format for DPI flag: %v", v)
+			}
+		}
+	}
+
+	return &dpi, nil
+}
+
 func main() {
 	flag.Parse()
 
@@ -79,6 +121,16 @@ func main() {
 		log.Fatalf("Invalid value for -profile2_light: %v", err)
 	}
 
+	dpi1, err := parseDPIFlag(*profile1DPI)
+	if err != nil {
+		log.Fatalf("Invalid value for -profile1_dpi: %v", err)
+	}
+
+	dpi2, err := parseDPIFlag(*profile2DPI)
+	if err != nil {
+		log.Fatalf("Invalid value for -profile2_dpi: %v", err)
+	}
+
 	device, err := hid.Open(HoltekVendorId, AnkerMouseDeviceId, "")
 	if err != nil {
 		log.Fatal(err)
@@ -88,9 +140,11 @@ func main() {
 	cfg.Profiles[0].LightProfile.SetColor(*c1)
 	cfg.Profiles[0].LightProfile.Brightness = bright1
 	cfg.Profiles[0].LightProfile.BreathSpeed = breath1
+	cfg.Profiles[0].DPIProfile.SetDPI(*dpi1)
 	cfg.Profiles[1].LightProfile.SetColor(*c2)
 	cfg.Profiles[1].LightProfile.Brightness = bright2
 	cfg.Profiles[1].LightProfile.BreathSpeed = breath2
+	cfg.Profiles[1].DPIProfile.SetDPI(*dpi2)
 
 	err = cfg.Write(device)
 	if err != nil {
